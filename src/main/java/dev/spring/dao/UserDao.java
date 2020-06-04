2f0e90fce487bc49c29b6a8d058006c4b19d6cdc
vladyslav <vlad.bes0711@gmail.com>
2020-06-04 14:30:03 +0300

    added mvc config and user controller

--- src/main/java/dev/spring/dao/UserDao.java
+++ src/main/java/dev/spring/dao/UserDao.java
@@ -2,9 +2,12 @@ package dev.spring.dao;
 
 import dev.spring.model.User;
 import java.util.List;
+import java.util.Optional;
 
 public interface UserDao {
     void create(User user);
 
     List<User> getAll();
+
+    Optional<User> getById(Long id);
 }
